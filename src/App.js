1b9fb551bfaa5c1b23cf0a572dbc1516330faaa9
Huseyin Daniel Ozdogan <huseyinozdogan@gmail.com>
2021-01-27 17:48:36 +0300

    WithRouter added,
    good to demonstrate router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { Component } from 'react';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import { Switch, Route} from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div >
-        <HomePage/>
-      </div>
-    );
-  }
+const HatsPage = () => (
+  <div>
+    <h1> Hats Page</h1>
+  </div>
+);
+
+function App() {
+  return (
+    <div>
+      <Switch>
+        <Route exact path='/' component={HomePage}/>
+        <Route path='/hats' component={HatsPage}/>
+      </Switch>
+    </div>
+  );
 }
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div >
+//         <HomePage/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
