e4034b51bf2789b492123851d46d17d5b1b071f3
Huseyin Daniel Ozdogan <huseyinozdogan@gmail.com>
2021-01-28 15:49:03 +0300

    header (navbar) added as component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component.jsx';
 
 
 function App() {
   return (
     <div>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
